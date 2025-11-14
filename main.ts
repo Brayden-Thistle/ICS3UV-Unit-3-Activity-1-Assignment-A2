52311b166dceaf5fbd57886fe1eee89f8868107d
Brayden Thistle <bthistle@huronperthcatholic.ca>
2025-11-14 16:31:37 +0000

    another typo fixed

--- main.ts
+++ main.ts
@@ -7,5 +7,4 @@
 
 //equation and answer using cube root.
 console.log("The length and width and height of a cube with a volume of 1000 mm³ is " + (Math.cbrt(1000)) + "mm");
-
 console.log("\nDone");
